62be99c46b0563585f7f18b5eb60f6dd41df29af
hen04 <nadia_degtiareva@rambler.ru>
2023-09-24 19:46:36 +0300

    doctors and doctor-item

--- src/js/main-script.js
+++ src/js/main-script.js
@@ -59,6 +59,8 @@ $(function (){
 	var dots = carousel.data('dots');
 	var auto = carousel.data('auto');
 	var loop = carousel.data('loop');
+	var items= carousel.data('items');
+	var slide= carousel.data('slide');
 	carousel.owlCarousel({
 		responsiveClass: true,
 		nav: nav,
@@ -68,22 +70,53 @@ $(function (){
 		autoHeight: false,
 		responsive: {
 			0: {
-				items: 1,
+				items: items,
 				margin: 20,
 				autoHeight: true,
 			},
 			768: {
 				margin: 20,
-				slideBy: 3,
-				items: 3,
+				slideBy: slide,
+				items: items,
 			},
 			1280: {
 				margin: 20,
-				slideBy: 2,
-				items: 2,
+				slideBy: slide,
+				items: items,
 			}
 		}
 	});
 
+	const headers = document.querySelectorAll('.tabs-header-item');
+	const tabs = document.querySelectorAll('.tabs-content-item');
+
+	function onTabClick(item) {
+		item.addEventListener('click', () => {
+			let currentHeader = item;
+			let tabId = currentHeader.getAttribute('data-tab');
+			let currentTab = document.querySelector(tabId);
+
+			if (!currentHeader.classList.contains('active')) {
+				headers.forEach((item) => {
+					item.classList.remove('active');
+				});
+
+				tabs.forEach((item) => {
+					item.classList.remove('active');
+				});
+
+				currentHeader.classList.add('active');
+				currentTab.classList.add('active');
+			}
+		});
+	}
+
+	headers.forEach(onTabClick);
+
+	$('.js-modal').on('click', function(event){
+		event.preventDefault();
+		var linkhref = $(this).attr('href');
+		$(linkhref).arcticmodal();
+	});
 
 });
